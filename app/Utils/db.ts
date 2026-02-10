976ac22b2ae9d53e38d30257e64680322c73b7d9
victorycesar <victorycesar0880@gmail.com>
2026-02-09 20:15:31 -0400

    crear pool, descargar mysql2 libreria

---
+++ app/Utils/db.ts
@@ -0,0 +1,11 @@
+import { createPool } from "mysql2/promise";
+
+export const pool = createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    database: process.env.DB_DATABASE,
+    password: process.env.DB_PASSWORD,
+    waitForConnections: true,
+    connectionLimit: 10,
+});
+
